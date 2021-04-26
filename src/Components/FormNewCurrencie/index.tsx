543fa80bb1f04f2fa28b9d545eef6ef2d5520268
glob <luisfelipegalleguillos@hotmail.com>
2021-04-26 00:22:08 -0400

    styles fix

--- src/Components/FormNewCurrencie/index.tsx
+++ src/Components/FormNewCurrencie/index.tsx
@@ -17,24 +17,33 @@ function FormNewCurrencie() {
 
     return(
         <div className="addCoin">
-        <select 
-            name="criptos" 
-            id="criptos-select" 
-            className="Select" 
-            value={option}
-            onChange={e => setOption(e.target.value)}
-        >
-          <option value="">Coin</option>
-          <option value="bitcoin">Bitcoin</option>
-          <option value="ethereum">Etherium</option>
-          <option value="binancecoin">Binance Coin</option>
-          <option value="chiliz">Chiliz</option>
-          <option value="polkadot">Polkadot</option>
-          <option value="uniswap">Uniswap</option>
-          <option value="vechain">VeChain</option>
-          <option value="chainlink">ChainLink</option>
-          <option value="monero">Monero</option>
-        </select>
+            <hr className="divisor"/>
+        <div className="InputContainer">
+            <select 
+                name="criptos" 
+                id="criptos-select" 
+                className="Select" 
+                value={option}
+                onChange={e => setOption(e.target.value)}
+                >
+            <option value="">Coin</option>
+            <option value="bitcoin">Bitcoin</option>
+            <option value="ethereum">Etherium</option>
+            <option value="binancecoin">Binance Coin</option>
+            <option value="chiliz">Chiliz</option>
+            <option value="polkadot">Polkadot</option>
+            <option value="uniswap">Uniswap</option>
+            <option value="vechain">VeChain</option>
+            <option value="chainlink">ChainLink</option>
+            <option value="monero">Monero</option>
+            <option value="ripple">Ripple</option>
+            <option value="cardano">Cardano</option>
+            <option value="dogecoin">Dogecoin</option>
+            <option value="litecoin">Litecoin</option>
+            <option value="pancakeswap-token">PancakeSwap</option>
+            <option value="blowfish">BlowFish</option>
+            </select>
+        </div>
         <Input
             type="number"
             value={price}
@@ -51,7 +60,9 @@ function FormNewCurrencie() {
             label="Amount"
             placeholder="0.005"
         />
-        <button type="submit" className="addCoinButton" onClick={handleSubmit}>+</button>
+        <div className="InputContainer">
+            <button type="submit" className="addCoinButton" onClick={handleSubmit}>+</button>
+        </div>
         </div>
     )
 }
